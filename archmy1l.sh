a76e7238c139543f49cd6ecbd69256795bca916a
MarcMilany <alecs1874@gmail.com>
2020-08-02 02:21:00 +0300

    Update archmy1l.sh

--- archmy1l.sh
+++ archmy1l.sh
@@ -1100,7 +1100,7 @@ echo -e "${BLUE}:: ${NC}Загрузка свежего списка зерка�
 # Команда отфильтрует пять зеркал, отсортирует их по скорости и обновит файл mirrorlist:
 #sudo pacman -Sy --noconfirm --noprogressbar --quiet reflector
 reflector --verbose --country 'Russia' -l 7 -p https -p http -n 7 --save /etc/pacman.d/mirrorlist --sort rate  
-#reflector --verbose --country 'Russia' -l 5 -p https -p http -n 5 --sort rate --save /etc/pacman.d/mirrorlist
+#reflector --verbose --country 'Russia' -l 7 -p https -p http -n 7 --sort rate --save /etc/pacman.d/mirrorlist
 # --------------------------------------------------------------------
 ############  Сделать  #############
 # ============================================================================
