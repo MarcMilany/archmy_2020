ce6d25a5062deb48b8f88db36bf0e7aca8c41c67
MarcMilany <alecs1874@gmail.com>
2020-08-09 00:52:21 +0300

    Update archmy1l.sh

--- archmy1l.sh
+++ archmy1l.sh
@@ -999,7 +999,7 @@ while
 echo " Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter") "
 read -p " 1 - Base + Base-Devel + packages, 2 - Base + packages, 3 - Base + Base-Devel, 4 - Base: " x_pacstrap  # To confirm the input actions, click 'Enter' ; # Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter")
 #echo " Действия ввода, выполняется сразу после нажатия клавиши "
-#read -n1 -p " 1 - Base + Base-Devel + packages, 2 - Base + packages, 3 - Base + Base-Devel, 4 - Base: " x_pacstrap  # sends right after the keypress; # отправляет сразу после нажатия клавиши
+# read -n1 -p " 1 - Base + Base-Devel + packages, 2 - Base + packages, 3 - Base + Base-Devel, 4 - Base: " x_pacstrap  # sends right after the keypress; # отправляет сразу после нажатия клавиши
     echo ''
     [[ "$x_pacstrap" =~ [^123456780] ]]
 do
@@ -1088,7 +1088,16 @@ echo -e "${YELLOW}==> ${NC}Установка производится в пор
 #echo 'Установка производится в порядке перечисления'
 # Installation Is performed in the order listed
 echo ""
-read -p " 1 - LINUX, 2 - LINUX_HARDENED, 3 - LINUX_LTS, 4 - LINUX_ZEN: " x_pacstrap  # To confirm the input actions, click 'Enter' ; # Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter")  
+while
+echo " Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter") "
+read -p " 1 - LINUX, 2 - LINUX_HARDENED, 3 - LINUX_LTS, 4 - LINUX_ZEN: " x_pacstrap  # To confirm the input actions, click 'Enter' ; # Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter")
+#echo " Действия ввода, выполняется сразу после нажатия клавиши "
+# read -n1 -p " 1 - LINUX, 2 - LINUX_HARDENED, 3 - LINUX_LTS, 4 - LINUX_ZEN: " x_pacstrap  # sends right after the keypress; # отправляет сразу после нажатия клавиши
+    echo ''
+    [[ "$x_pacstrap" =~ [^123456780] ]]
+do
+    :
+done  
  if [[ $x_pacstrap == 1 ]]; then
   clear
   echo ""
