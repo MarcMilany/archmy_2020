246a111e3223f3263a3b4189dddb2b22e2de51aa
MarcMilany <alecs1874@gmail.com>
2020-08-10 14:45:09 +0300

    Update archmy1l.sh

--- archmy1l.sh
+++ archmy1l.sh
@@ -1043,7 +1043,10 @@ echo " Чтобы исключить ошибки в работе системы
 echo ""
 while 
 echo " Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter") "
-read -p " 1 - Base + Base-Devel + packages, 2 - Base + packages, 3 - Base + Base-Devel, 4 - Base: " x_pacstrap  # To confirm the input actions, click 'Enter' ; # Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter")
+#read -p " 1 - Base + Base-Devel + packages, 2 - Base + packages, 3 - Base + Base-Devel, 4 - Base: " x_pacstrap  # To confirm the input actions, click 'Enter' ; # Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter")
+read -p " 1 - Base + Base-Devel + packages,  2 - Base + packages, 
+           
+          3 - Base + Base-Devel,             4 - Base: " x_pacstrap  # To confirm the input actions, click 'Enter' ; # Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter")
 #echo " Действия ввода, выполняется сразу после нажатия клавиши "
 # read -n1 -p " 1 - Base + Base-Devel + packages, 2 - Base + packages, 3 - Base + Base-Devel, 4 - Base: " x_pacstrap  # sends right after the keypress; # отправляет сразу после нажатия клавиши
     echo ''
