38d23e4429c713d3c9f011e1cb693061b87dbc3d
MarcMilany <alecs1874@gmail.com>
2020-08-12 23:19:38 +0300

    Update archmy2l.sh

--- archmy2l.sh
+++ archmy2l.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#
+
 #### Смотрите пометки (справочки) и доп.иформацию в самом скрипте! #### 
-#
+
 apptitle="Arch Linux Fast Install v1.6 LegasyBIOS - Version: 2020.07.16.00.40.38 (GPLv3)"
 baseurl=https://raw.githubusercontent.com/MarcMilany/archmy_2020/master/url%20links%20abbreviated/git%20url
 cpl=0
@@ -10,7 +10,7 @@ fspkgs=""
 EDITOR=nano
 # Выполните команду с правами суперпользователя:
 #EDITOR=nano visudo
-#
+
 ##################################################################
 ##### <<<Arch Linux Fast Install LegasyBIOS (arch2020)>>>    #####
 #### Скрипты 'arch_2020' созданы на основе сценария (скрипта) ####
