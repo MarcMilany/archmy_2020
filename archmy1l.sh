1a1a927d9314f19fb54f8e5e6896ab62ef419906
MarcMilany <alecs1874@gmail.com>
2020-08-15 03:29:48 +0300

    Update archmy1l.sh

--- archmy1l.sh
+++ archmy1l.sh
@@ -1529,7 +1529,8 @@ echo -e "${BLUE}       ARCH LINUX FAST INSTALL ${RED}1.6 Update${NC}"
 echo " ############################################### "
 echo " Размонтирование всех смонтированных файловых систем (кроме корневой) "
 umount -a
-reboot 
+#reboot
+shutdown -r now 
   elif [[ $int == 2 ]]; then
   echo ""
   pacman -S wget --noconfirm --noprogressbar 
