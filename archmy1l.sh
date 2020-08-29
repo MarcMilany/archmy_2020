b8720422b4d245924b366b7003fccbcef8d6880a
MarcMilany <alecs1874@gmail.com>
2020-08-29 16:34:53 +0300

    Update archmy1l.sh

--- archmy1l.sh
+++ archmy1l.sh
@@ -1126,13 +1126,14 @@ echo " Чтобы исключить ошибки в работе системы
 # To eliminate errors in the system, I recommend "1"
 echo ""
 while 
-echo " Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter") "
-    read -p "    
+echo " Действия ввода, выполняется сразу после нажатия клавиши "
+    read -n1 -p "    
     1 - Base + Base-Devel + packages,   2 - Base + packages, 
 
-    3 - Base + Base-Devel,              4 - Base: " x_pacstrap  # To confirm the input actions, click 'Enter' ; # Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter")
-#echo " Действия ввода, выполняется сразу после нажатия клавиши "
-# read -n1 -p " 1 - Base + Base-Devel + packages, 2 - Base + packages, 3 - Base + Base-Devel, 4 - Base: " x_pacstrap  # sends right after the keypress; # отправляет сразу после нажатия клавиши
+    3 - Base + Base-Devel,              4 - Base: " x_pacstrap  # sends right after the keypress; # отправляет сразу после нажатия клавиши
+# echo " Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter") "    
+# read -p " 1 - Base + Base-Devel + packages, 2 - Base + packages, 3 - Base + Base-Devel, 4 - Base: " x_pacstrap  
+# To confirm the input actions, click 'Enter' ; # Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter")
     echo ''
     [[ "$x_pacstrap" =~ [^123456780] ]]
 do
