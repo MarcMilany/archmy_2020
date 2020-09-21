30c230bdb1ce46c6aeeeb5bb57e669c7418121d0
MarcMilany <alecs1874@gmail.com>
2020-09-21 08:38:44 +0300

    Update archmy3l.sh

--- archmy3l.sh
+++ archmy3l.sh
@@ -157,7 +157,7 @@ ${BLUE}===> ******************************************************* ${NC}"
 ### Display banner (Дисплей баннер)
 _warning_banner
 
-sleep 5
+sleep 7
 #echo -e "${MAGENTA}==> ${BOLD}Если у Вас беспроводное соединение, запустите nmtui и подключитесь к сети. ${NC}"
 #echo 'Если у Вас беспроводное соединение, запустите nmtui и подключитесь к сети.'
 # If you have a wireless connection, launch nmtui and connect to the network.
