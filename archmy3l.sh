a9a6d3de5a8be0ebbdf891bc7348c57c182ccdbe
MarcMilany <alecs1874@gmail.com>
2020-09-21 21:25:01 +0300

    Update archmy3l.sh

--- archmy3l.sh
+++ archmy3l.sh
@@ -658,6 +658,8 @@ fi
 # https://wiki.archlinux.org/index.php/CUPS_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)
 # CUPS (Русский)/Printer-specific problems (Русский)
 # https://wiki.archlinux.org/index.php/CUPS_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)/Printer-specific_problems_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)
+# Arch Linux: cups и hplip - подключение принтера
+# https://rtfm.co.ua/arch-linux-cups-i-hplip-podklyuchenie-printera/
 # ------------------------------------------------------------------------
 
 echo ""
