d6c72f6ddf2e2ebe071863e9d6b0a19da3c8d74c
MarcMilany <alecs1874@gmail.com>
2020-09-23 02:25:14 +0300

    yay-bin-install.sh

---
+++ AUR helper/yay-bin-install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Install script yay-bin
+# autor: Alex Creio https://cvc.hashbase.io/
+
+# wget git.io/yay-bin-install.sh && sh yay-bin-install.sh
+
+echo -e " Установка базовых программ и пакетов wget, curl, git "
+sudo pacman -S --noconfirm --needed wget curl git 
+echo " Установка "AUR Helper" (yay-bin) "
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+# makepkg -si
+# makepkg -si --noconfirm   #-не спрашивать каких-либо подтверждений
+makepkg -si --skipinteg
+cd ..
+rm -rf yay-bin
+# rm -Rf yay-bin
+echo " Установка AUR Helper (yay-bin) завершена "
+
+#
+# https://aur.archlinux.org/packages/yay-git/
+# Йогурт (yogurt). Обертка Pacman и помощник AUR, написанные на языке go. (версия в разработке)
+# База пакета: yay-git
+# URL восходящего направления:	https://github.com/Jguer/yay
+# Git Clone URL: https://aur.archlinux.org/yay-git.git (только для чтения, нажмите, чтобы скопировать)
+# Популярность:	0.67
+# Первый отправленный:	2018-01-29 05:52
+# Последнее обновление:	2020-08-18 17:28
+#
+# Если в системе не установлены необходимые зависимости, makepkg предупредит вас об этом и отменит сборку. Если задать флаг -s/--syncdeps, то makepkg самостоятельно установит недостающие зависимости и соберёт пакет.
+# $ makepkg --syncdeps
+# 
