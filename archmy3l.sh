53442cd6e113520c60b64a38713cc0387eff9b18
MarcMilany <alecs1874@gmail.com>
2020-10-01 02:29:23 +0300

    Update archmy3l.sh

--- archmy3l.sh
+++ archmy3l.sh
@@ -850,7 +850,7 @@ echo -e "${BLUE}:: ${NC}Установка Мультимедиа кодеков
 sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-bad gst-plugins-ugly libdvdcss libdvdread libdvdnav dvd+rw-tools dvdauthor dvgrab cdrdao gst-libav gst-libav gpac --noconfirm
 
 echo ""
-echo -e "${BLUE}:: ${NC}Установка Мультимедиа утилит" 
+echo -e "${BLUE}:: ${NC}Установка Мультимедиа утилит (пакетов)" 
 #echo 'Установка Мультимедиа утилит'
 # Installing Multimedia utilities
 sudo pacman -S audacity audacious audacious-plugins smplayer smplayer-skins smplayer-themes smtube deadbeef easytag subdownloader mediainfo-gui vlc --noconfirm
