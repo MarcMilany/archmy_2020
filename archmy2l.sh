6e059058e6f75242974dce168fefab0e4dcb0c61
MarcMilany <alecs1874@gmail.com>
2020-10-04 11:25:07 +0300

    Update archmy2l.sh

--- archmy2l.sh
+++ archmy2l.sh
@@ -1914,6 +1914,8 @@ if [[ $prog_set == 0 ]]; then
 echo ""  
 echo " Создание каталогов пропущено "
 elif [[ $prog_set == 1 ]]; then
+echo ""  
+echo " Создание пользовательских каталогов по умолчанию "     
  pacman -S xdg-user-dirs --noconfirm
  xdg-user-dirs-update 
  echo "" 
