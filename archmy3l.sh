79a9898ecd42df6d16fbc88166cab7049bfbdf07
MarcMilany <alecs1874@gmail.com>
2020-10-04 14:48:35 +0300

    Update archmy3l.sh

--- archmy3l.sh
+++ archmy3l.sh
@@ -3243,6 +3243,7 @@ echo -e "${GREEN}
 #sudo tar -xzf master.zip -C ~/ 
 #sudo tar -xzf arch_2020-master.zip -C ~/
 #git clone https://github.com/MarcMilany/arch_2020.git
+wget https://raw.githubusercontent.com/MarcMilany/archmy_2020/master/Config/Additional%20settings%20for%20Xfce%20(%D0%94%D0%BE%D0%BF%D0%BE%D0%BB%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5%20%D0%9D%D0%B0%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B8)
 
 echo -e "${BLUE}:: ${NC}Посмотрим дату и время без характеристик для проверки времени"
 #echo 'Посмотрим дату и время без характеристик для проверки времени'
