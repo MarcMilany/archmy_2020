de4889462f35b286d2add16af62fea5f2db267c8
MarcMilany <alecs1874@gmail.com>
2020-10-06 07:38:12 +0300

    Update archmy3l.sh

--- archmy3l.sh
+++ archmy3l.sh
@@ -1922,16 +1922,16 @@ echo ""
 while  
 echo " Действия ввода, выполняется сразу после нажатия клавиши "
     read -n1 -p "      
-    1 - Да установить,     0 - НЕТ - Пропустить установку: " prog_set  # sends right after the keypress; # отправляет сразу после нажатия клавиши
+    1 - Да установить,     0 - НЕТ - Пропустить установку: " i_keyboard  # sends right after the keypress; # отправляет сразу после нажатия клавиши
     echo ''
-    [[ "$prog_set" =~ [^10] ]]
+    [[ "$i_keyboard" =~ [^10] ]]
 do
     :
 done 
-if [[ $prog_set == 0 ]]; then 
+if [[ $i_keyboard == 0 ]]; then 
 echo ""   
 echo " Установка утилит (пакетов) пропущена "
-elif [[ $prog_set == 1 ]]; then
+elif [[ $i_keyboard == 1 ]]; then
   echo ""  
   echo " Установка Onboard (экранной клавиатуры) "
 sudo pacman -S onboard --noconfirm  # Экранная клавиатура
