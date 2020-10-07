4543f1899168b7e30e3d7b2b030afb778b15c01b
MarcMilany <alecs1874@gmail.com>
2020-10-07 06:11:13 +0300

    Update archmy2l.sh

--- archmy2l.sh
+++ archmy2l.sh
@@ -1885,16 +1885,16 @@ echo ""
 while 
 echo " Действия ввода, выполняется сразу после нажатия клавиши "
     read -n1 -p " 
-    1 - Да удалить зависимость 'go',     0 - Нет пропустить этот шаг: " prog_tool  # sends right after the keypress; # отправляет сразу после нажатия клавиши     
+    1 - Да удалить зависимость 'go',     0 - Нет пропустить этот шаг: " rm_tool  # sends right after the keypress; # отправляет сразу после нажатия клавиши     
     echo ''
-    [[ "$prog_tool" =~ [^10] ]]
+    [[ "$rm_tool" =~ [^10] ]]
 do
     :
 done
-if [[ $prog_tool == 0 ]]; then
+if [[ $rm_tool == 0 ]]; then
 echo ""
 echo " Удаление зависимости 'go' пропущено "      
-elif [[ $prog_tool == 1 ]]; then
+elif [[ $rm_tool == 1 ]]; then
 # sudo pacman -Rs go
 #pacman -Rs go
 pacman --noconfirm -Rs go    # --noconfirm  --не спрашивать каких-либо подтверждений
