05588760740401a028aaa94369603f5073083262
MarcMilany <alecs1874@gmail.com>
2020-10-07 07:03:34 +0300

    Update archmy3l.sh

--- archmy3l.sh
+++ archmy3l.sh
@@ -1177,16 +1177,16 @@ echo ""
 while  
 echo " Действия ввода, выполняется сразу после нажатия клавиши "
     read -n1 -p "      
-    1 - Да установить,     0 - НЕТ - Пропустить установку: " prog_set  # sends right after the keypress; # отправляет сразу после нажатия клавиши
+    1 - Да установить,     0 - НЕТ - Пропустить установку: " i_vlc  # sends right after the keypress; # отправляет сразу после нажатия клавиши
     echo ''
-    [[ "$prog_set" =~ [^10] ]]
+    [[ "$i_vlc" =~ [^10] ]]
 do
     :
 done 
-if [[ $prog_set == 0 ]]; then 
+if [[ $i_vlc == 0 ]]; then 
 echo ""   
 echo " Установка утилит (пакетов) пропущена "
-elif [[ $prog_set == 1 ]]; then
+elif [[ $i_vlc == 1 ]]; then
   echo ""  
   echo " Установка многоплатформенного проигрывателя VLC "
 sudo pacman -S vlc --noconfirm
