41e6859116ee32caf5efbdc4f87f00037dc4ec60
MarcMilany <alecs1874@gmail.com>
2020-10-10 10:31:48 +0300

    Update archmy3l.sh

--- archmy3l.sh
+++ archmy3l.sh
@@ -893,9 +893,9 @@ elif [[ $i_sound == 1 ]]; then
 sudo pacman -S alsa-utils alsa-plugins alsa-firmware alsa-lib alsa-utils --noconfirm 
 sudo pacman -S lib32-alsa-plugins --noconfirm  # Дополнительные плагины ALSA (32-бит)
 sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol pulseaudio-bluetooth pulseaudio-equalizer-ladspa --noconfirm
-sudo pacman -S pulseaudio-zeroconf --noconfirm   # Поддержка Zeroconf для PulseAudio
-sudo pacman -S pasystray --noconfirm   # Системный трей PulseAudio (замена # padevchooser)  
-sudo pacman -S xfce4-pulseaudio-plugin --noconfirm 
+sudo pacman -S pulseaudio-zeroconf --noconfirm  # Поддержка Zeroconf для PulseAudio
+sudo pacman -S pasystray --noconfirm  # Системный трей PulseAudio (замена # padevchooser)  
+sudo pacman -S xfce4-pulseaudio-plugin --noconfirm  # Плагин Pulseaudio для панели Xfce4 
 #sudo pacman -Sy pavucontrol pulseaudio-bluetooth alsa-utils pulseaudio-equalizer-ladspa --noconfirm
 clear
 echo ""   
