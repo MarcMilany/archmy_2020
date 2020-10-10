32fe5e5d8f64d4a914c1b06372240ec6b7452b61
MarcMilany <alecs1874@gmail.com>
2020-10-10 22:15:14 +0300

    Update archmy3l.sh

--- archmy3l.sh
+++ archmy3l.sh
@@ -917,7 +917,7 @@ sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol pulseaudio-bluetooth pulse
 #sudo pacman -S pulseaudio-equalizer-ladspa --noconfirm  # 15-полосный эквалайзер для PulseAudio
 ### sudo pacman -S pulseaudio-equalizer --noconfirm  # Графический эквалайзер для PulseAudio
 sudo pacman -S pulseaudio-zeroconf --noconfirm  # Поддержка Zeroconf для PulseAudio
-sudo pacman -S pasystray --noconfirm  # Системный трей PulseAudio (замена # padevchooser)  
+#sudo pacman -S pasystray --noconfirm  # Системный трей PulseAudio (замена # padevchooser)  
 sudo pacman -S xfce4-pulseaudio-plugin --noconfirm  # Плагин Pulseaudio для панели Xfce4 
 #sudo pacman -Sy pavucontrol pulseaudio-bluetooth alsa-utils pulseaudio-equalizer-ladspa --noconfirm
 clear
