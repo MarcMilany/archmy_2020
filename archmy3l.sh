71d969fbe228eb9b4420bf4deb6c0ad8bcb49356
MarcMilany <alecs1874@gmail.com>
2020-10-10 11:04:17 +0300

    Update archmy3l.sh

--- archmy3l.sh
+++ archmy3l.sh
@@ -859,6 +859,10 @@ sudo pacman -S bluez bluez-libs bluez-cups bluez-utils --noconfirm
 echo ""   
 echo " Установка пакетов поддержки Bluetooth выполнена "
 fi
+# -----------------------------------------
+# Bluetooth (Русский)
+# https://wiki.archlinux.org/index.php/Bluetooth_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)
+# ==========================================
 
 clear
 echo ""
