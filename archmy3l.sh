ce9486e7c9d0b741f58138c80c4a3298ebc5e3de
MarcMilany <alecs1874@gmail.com>
2020-10-19 20:48:39 +0300

    Update archmy3l.sh

--- archmy3l.sh
+++ archmy3l.sh
@@ -3651,6 +3651,7 @@ sudo pacman -S ffmpegthumbnailer --noconfirm  # Легкий эскиз виде
 sudo pacman -S poppler-glib --noconfirm  # Наручники Poppler Glib
 sudo pacman -S libgsf --noconfirm  # Расширяемая библиотека абстракции ввода-вывода для работы со структурированными форматами файлов
 sudo pacman -S libopenraw --noconfirm  # Библиотека для декодирования файлов RAW
+sudo pacman -S shared-mime-info --noconfirm  # Общая информация MIME на Freedesktop.org
 ############ gstreamer0.10 ##########
 echo "" 
 echo " Установим мультимедийный фреймворк GStreamer из AUR "
@@ -3684,20 +3685,53 @@ sudo update-mime-database /usr/share/mime
 echo " Желательно ПОСЛЕ этих действий выйдите из системы и снова войдите в систему, или перезагрузитесь "  # Then logout and back in or Reboot
 echo " Но, мы просто перезапустим файловый менеджер Thunar "
 thunar -q # запустим менеджер thunar
-echo " Проверяем дефолтные настройки "
-# Проверяем дефолтные настройки:
-# xdg-mime query default inode/directory
-
-
+echo ""
+echo " Проверяем дефолтные настройки миниатюр (xdg-mime query default) "
+xdg-mime query default inode/directory
+echo " Переопределяем на файловый менеджер Thunar "
+xdg-mime default thunar.desktop inode/directory
+# Чтобы открыть каталог и выбрать подкаталог / файл в thunar:
+# thunar --select path/to/file/or/directory
+### xdg-mime default org.gnome.Nautilus.desktop inode/directory  # или Nautilus
+# Чтобы открыть каталог и выбрать подкаталог / файл в наутилусе:
+### nautilus --select path/to/file/or/directory
+echo " Расширяем контекстное меню Thunar (Добавляем дополнительные пункты для создания файлов) "
+XDG_TEMPLATES_DIR=$(xdg-user-dir TEMPLATES)
+cd "$XDG_TEMPLATES_DIR"
+touch 'New Text File.txt' && touch 'New Word File.doc' && touch 'New Excel Spreadsheet.xls'
+pwd    # покажет в какой директории мы находимся
+cd ..   # поднимаемся на уровень выше (выходим из папки сборки)
+fi
+# ---------------------------------
 # Затем, выставить в настройках Thunar галочку, предписывающую показывать миниатюры, если это возможно!
 # Почистить cache:
 # /home/user/.thumbnails
 # /home/user/.cache/Thunar
-fi
-# ---------------------------------
 # update-mime-database  # программа для построения кэша Shared MIME-Info базы данных
 # Это программа, которая отвечает за обновление общего кэша информации mime в соответствии с системой, описанной в спецификации Shared MIME-Info Database от X Desktop Group
 #/usr/share/mime  # файл конфигурации MIME-типов
+# ----------------------------
+# https://wiki.archlinux.org/index.php/Thunar_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)
+# https://www.opennet.ru/man.shtml?topic=update-mime-database&category=1&russian=2
+# https://unix.stackexchange.com/questions/364997/open-a-directory-in-the-default-file-manager-and-select-a-file
+# https://specifications.freedesktop.org/mime-apps-spec/mime-apps-spec-1.0.1.html
+# ----------------------------------
+# Запросы
+# xdg-mime query filetype <filename> позволяет узнать mime-тип файла.
+# xdg-mime query default <mime-type> – узнать приложение по умолчанию для открытия данного mime-типа.
+# Установка умолчаний
+# xdg-mime default <desktop-file-name> <mime-type>... позволяет установить приложение <desktop-file-name> по умолчанию для открытия одного или нескольких mime-типов. <desktop-file-name> – это название desktop-файла, который будет использован (без пути, с расширением)
+# Например:
+# xdg-mime default okular.desktop image/jpeg image/png
+# В случае, если возможно установить окружение рабочего стола, для этого будут использованы средства окружения. Иначе, будут добавлены записи в файл mimeapps.list (первый найденный) в секцию Default Applications.
+# РЕКОМЕНДАЦИИ
+# Всегда устанавливайте переменную BROWSER. Если она не установлена, можно ждать неожиданностей. Можно указывать список, разделённый двоеточием :.
+# Используйте xdg-mime default для установки приложений по умолчанию. Если требуется посмотреть список умолчаний, можно посмотреть в файл mimeapps.list, однако редактировать его напрямую в общем случае не рекомендуется.
+# https://wiki.archlinux.org/index.php/Default_applications_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)
+# Общая база данных MIME-информации
+# https://specifications.freedesktop.org/shared-mime-info-spec/shared-mime-info-spec-0.11.html#idm139839923550176
+# Небольшая заметка про xdg-open
+# https://livid.pp.ru/posts/2016-03-08-xdg-open.html
 # ----------------------------
 # Ассоциации файлов
 # Это нужно, если у вас открывается файл, или каталог не в той программе. Например, директория в музыкальном проигрывателе.
@@ -3718,23 +3752,10 @@ fi
 ### xdg-mime default vlc.desktop video/mp4
 # Нестандартная ассоциация:
 # Приложения могут игнорировать или частично реализовывать стандарт XDG. Проверьте использование устаревших файлов, таких как ~/.local/share/applications/mimeapps.list и ~/.local/share/applications/defaults.list. Если вы пытаетесь открыть файл из другого приложения (например, веб-браузера или файлового менеджера), проверьте, имеет ли это приложение собственный способ выбора приложений по умолчанию.
+# База данных MIME
+# Система поддерживает базу данных распознанных типов MIME: Общая база данных MIME. База данных построена из файлов XML, установленных пакетами в /usr/share/mime/packages, используя инструменты из shared-mime-info.
+# Файлы в /usr/share/mime/ не должны редактироваться напрямую, однако их можно сохранить в отдельную базу данных для каждого пользователя в ~/.local/share/mime/.
 # ----------------------------------
-# Проверяем дефолтные настройки:
-# xdg-mime query default inode/directory
-# Переопределяем:
-### xdg-mime default org.gnome.Nautilus.desktop inode/directory
-# или thunar
-# xdg-mime default thunar.desktop inode/directory
-# https://wiki.archlinux.org/index.php/Default_applications_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)
-# -----------------------------
-# Расширяем контекстное меню thunar:
-# Добавляем дополнительные пункты для создания файлов.
-# XDG_TEMPLATES_DIR=$(xdg-user-dir TEMPLATES)
-# cd "$XDG_TEMPLATES_DIR"
-# touch 'New Text File.txt' && touch 'New Word File.doc' && touch 'New Excel Spreadsheet.xls'
-# https://wiki.archlinux.org/index.php/Thunar_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)
-# https://www.opennet.ru/man.shtml?topic=update-mime-database&category=1&russian=2
-# ==================================
 
 clear
 echo -e "${CYAN}
