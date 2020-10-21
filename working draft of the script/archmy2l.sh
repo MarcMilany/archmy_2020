b6a2b794d792c7c64eabea37f962620a04cdec03
MarcMilany <alecs1874@gmail.com>
2020-10-21 16:44:15 +0300

    Update archmy2l.sh

--- working draft of the script/archmy2l.sh
+++ working draft of the script/archmy2l.sh
@@ -744,7 +744,7 @@ echo ""
 # Если вы используете LVM для вашего /boot, вы можете установить GRUB на нескольких физических дисках.
  echo " Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter") "
  read -p " => Укажите диск куда установить GRUB (sda/sdb например sda или sdb) : " x_cfd  # To confirm the input actions, click 'Enter' ; # Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter")
- grub-install /dev/$x_cfd   #sda sdb sdc sdd
+ grub-install /dev/$x_cfd   #sda sdb sdc sdd # Записываем загрузчик в MBR (Master Boot Record) нашего внутреннего накопителя
 #grub-install --recheck /dev/$x_cfd     # Если Вы получили сообщение об ошибке
 #grub-install --boot-directory=/mnt/boot /dev/$x_cfd  # установить файлы загрузчика в другой каталог
   echo " Загрузчик GRUB установлен на выбранный вами диск (раздел). " 
@@ -765,7 +765,7 @@ echo ""
  echo " Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter") "
  read -p " => Укажите диск куда установить GRUB (sda/sdb например sda или sdb) : " x_cfd # To confirm the input actions, click 'Enter' ; # Чтобы подтвердить действия ввода, нажмите кнопку 'Ввод' ("Enter")
 # Если нужно установить BIOS-версию загрузчика из-под системы, загруженной в режиме UEFI
- grub-install --target=i386-pc /dev/$x_cfd   #sda sdb sdc sdd
+ grub-install --target=i386-pc /dev/$x_cfd   #sda sdb sdc sdd # Записываем загрузчик в MBR (Master Boot Record) нашего внутреннего накопителя
 #grub-install --target=i386-pc --recheck /dev/$x_cfd   # Если Вы получили сообщение об ошибке
   echo " Загрузчик GRUB установлен на выбранный вами диск (раздел). " 
 #grub-mkconfig -o /boot/grub/grub.cfg
