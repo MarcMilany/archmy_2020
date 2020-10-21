e5d760a5cae2e49abeb28a5a87b55c92421826de
MarcMilany <alecs1874@gmail.com>
2020-10-21 11:19:11 +0300

    Update archmy4.sh

--- archmy4.sh
+++ archmy4.sh
@@ -179,6 +179,8 @@ sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman-key --refresh-keys
 sudo pacman -S archlinux-keyring
+###  sudo pacman -S archlinux-keyring  # (обновление пакета ключей подписи)
+### sudo pacman -Sy archlinux-keyring
 sudo pacman -S seahorse  # Приложение GNOME для управления ключами PGP (управления паролями и ключами шифрования)
 sudo pacman -Syyu
 # ============================================================================
