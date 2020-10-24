0e46447d0aac8ff57138f45b4085ac9161a36760
MarcMilany <alecs1874@gmail.com>
2020-10-25 01:17:19 +0300

    Update archmy3l.sh

--- archmy3l.sh
+++ archmy3l.sh
@@ -2472,7 +2472,7 @@ echo -e "${GREEN}==> ${BOLD}Установить рекомендованные 
 #echo -e "${BLUE}:: ${NC}Установить рекомендованные программы (пакеты)?"
 #echo 'Установить рекомендованные программы?'
 # Install the recommended programs
-echo -e "${MAGENTA}=> ${NC}Список утилит (пакетов) для установки: - (gnome-system-monitor, gnome-disk-utility, frei0r-plugins, clonezilla, psensor, copyq, grsync, numlockx, modem-manager-gui, rofi, gsmartcontrol, testdisk, lsof, dmidecode)."
+echo -e "${MAGENTA}=> ${NC}Список утилит (пакетов) для установки: - (gnome-system-monitor, gnome-disk-utility, frei0r-plugins, clonezilla, psensor, copyq, rsync, grsync, numlockx, modem-manager-gui, rofi, gsmartcontrol, testdisk, lsof, dmidecode)."
 echo " Будьте внимательны! Процесс установки, был прописан полностью автоматическим. " 
 # Be careful! The installation process was fully automatic
 echo " Если Вы сомневаетесь в своих действиях, ещё раз обдумайте... "
@@ -2494,7 +2494,7 @@ echo " Установка утилит (пакетов) пропущена "
 elif [[ $i_collection == 1 ]]; then
   echo ""   
   echo " Установка рекомендованных утилит (пакетов) "
-sudo pacman -S gnome-system-monitor gnome-disk-utility frei0r-plugins clonezilla psensor copyq grsync numlockx modem-manager-gui rofi gsmartcontrol testdisk lsof dmidecode --noconfirm  
+sudo pacman -S gnome-system-monitor gnome-disk-utility frei0r-plugins clonezilla psensor copyq rsync grsync numlockx modem-manager-gui rofi gsmartcontrol testdisk lsof dmidecode --noconfirm  
 echo ""   
 echo " Установка утилит (пакетов) выполнена "
 fi
