5bd7e9962a632a9502d6a3d8c2c9f419bf562efd
MarcMilany <alecs1874@gmail.com>
2021-04-03 10:49:38 -0400

    Update design_for_xfce.sh

--- Design for Xfce/design_for_xfce.sh
+++ Design for Xfce/design_for_xfce.sh
@@ -818,6 +818,7 @@ echo ""
 ### https://aur.archlinux.org/faba-icon-theme-git.git
 ### https://github.com/snwh/faba-icon-theme
 ### https://snwh.org/
+### Последнее обновление:	2016-02-20 22:39
 ###
 ### papirus-smplayer-theme-git  AUR  # Тема Papirus для SMPlayer (версия git)  
 ### https://aur.archlinux.org/packages/papirus-smplayer-theme-git/ 
