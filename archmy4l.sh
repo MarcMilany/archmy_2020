22b892e42a62c13d5cd1206f690040c427d74de5
MarcMilany <alecs1874@gmail.com>
2021-04-09 18:42:09 -0400

    Update archmy4l.sh

--- archmy4l.sh
+++ archmy4l.sh
@@ -1348,8 +1348,18 @@ echo ""
 #exit(1)  # означает, что была какая-то ошибка (проблема), и именно поэтому программа выходит
 
 
-
-
+##### Шпаргалка запуска необходимых служб #####
+### systemctl enable NetworkManager
+### systemctl enable bluetooth
+### systemctl enable cups.service
+### systemctl enable sshd
+### systemctl enable avahi-daemon
+### systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+### systemctl enable reflector.timer
+### systemctl enable fstrim.timer
+### systemctl enable libvirtd
+### systemctl enable firewalld
+### systemctl enable acpid
 
 
 
