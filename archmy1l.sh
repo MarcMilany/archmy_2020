60b9e11b8f6fe407bd8cd43d4650dae5f5526479
MarcMilany <alecs1874@gmail.com>
2021-05-07 04:45:41 -0400

    Update archmy1l.sh

--- archmy1l.sh
+++ archmy1l.sh
@@ -44,76 +44,10 @@ script_path=$(readlink -f ${0%/*})
 umask 0022 # Определение окончательных прав доступа - Для суперпользователя (root) umask по умолчанию равна 0022
 set -e "\n${RED}Error: ${YELLOW}${*}${NC}"  # Эта команда остановит выполнение сценария после сбоя команды и будет отправлен код ошибки
 
-### Help and usage (--help or -h) (Справка)
-_help() {
-    echo -e "${BLUE}
-Installation guide - Arch Wiki
-${BOLD}For more information, see the wiki: \
-${GREY}<https://wiki.archlinux.org/index.php/Installation_guide>${NC}"
-}
-
 ### SHARED VARIABLES AND FUNCTIONS (ОБЩИЕ ПЕРЕМЕННЫЕ И ФУНКЦИИ)
 ### Shell color codes (Цветовые коды оболочки)
 RED="\e[1;31m"; GREEN="\e[1;32m"; YELLOW="\e[1;33m"; GREY="\e[3;93m"
 BLUE="\e[1;34m"; CYAN="\e[1;36m"; BOLD="\e[1;37m"; MAGENTA="\e[1;35m"; NC="\e[0m"
-
-### Automatic error detection (Автоматическое обнаружение ошибок)
-_set() {
-    set [--abefhkmnptuvxBCHP] [-o option] [arg ...]
-}
-
-_set() {
-    set -e "\n${RED}Error: ${YELLOW}${*}${NC}"
-    _note "${MSG_ERROR}"
-    sleep 1; $$
-}
-  
-### Display install steps (Отображение шагов установки)
-_info() {
-    echo -e "${YELLOW}\n==> ${CYAN}${1}...${NC}"; sleep 1
-}
-
-### Download show progress bar only (Скачать показывать только индикатор выполнения)
-_wget() {
-    wget "${1}" --quiet --show-progress
-}
-
-### Execute action in chrooted environment (Выполнение действия в хромированной среде)
-_chroot() {
-    arch-chroot /mnt <<EOF "${1}"
-EOF
-}
-
-### Display error, cleanup and kill (Ошибка отображения, очистка и убийство)
-_error() {
-    echo -e "\n${RED}Error: ${YELLOW}${*}${NC}"
-    _note "${MSG_ERROR}"
-    sleep 1; _cleanup; _exit_msg; kill -9 $$
-}
-
-### Cleanup on keyboard interrupt (Очистка при прерывании работы клавиатуры)
-_trap() {
-trap '_error ${MSG_KEYBOARD}' 1 2 3 6
-}
-#trap "set -$-" RETURN; set +o nounset
-# Или
-#trap "set -${-//[is]}" RETURN; set +o nounset
-#..., устраняя недействительные флаги и действительно решая эту проблему!
-
-### Reboot with 10s timeout (Перезагрузка с таймаутом 10 секунд)
-_reboot() {
-    for (( SECOND=10; SECOND>=1; SECOND-- )); do
-        echo -ne "\r\033[K${GREEN}${MSG_REBOOT} ${SECOND}s...${NC}"
-        sleep 1
-    done
-    reboot; exit 0
-}
-
-### Say goodbye (Распрощаться)
-_exit_msg() {
-    echo -e "\n${GREEN}<<< ${BLUE}${APPNAME} ${VERSION} ${BOLD}by \
-${AUTHOR} ${RED}under ${LICENSE} ${GREEN}>>>${NC}"""
-}
 
 #########   Baner  ####################
 #_arch_fast_install_banner
