077898eb4339b159c3aea5fe9995c7b06f94f7b4
MarcMilany <alecs1874@gmail.com>
2021-05-07 04:48:11 -0400

    Update archmy2l.sh

--- archmy2l.sh
+++ archmy2l.sh
@@ -16,47 +16,14 @@ umask 0022 # Определение окончательных прав дост
 set -e "\n${RED}Error: ${YELLOW}${*}${NC}"  # Эта команда остановит выполнение сценария после сбоя команды и будет отправлен код ошибки
 
 ### Help and usage (--help or -h) (Справка)
-_help() {
-    echo -e "${BLUE}
-Installation guide - Arch Wiki
-${BOLD}For more information, see the wiki: \
-${GREY}<https://wiki.archlinux.org/index.php/Installation_guide>${NC}"
-}
 
 ### SHARED VARIABLES AND FUNCTIONS (ОБЩИЕ ПЕРЕМЕННЫЕ И ФУНКЦИИ)
 ### Shell color codes (Цветовые коды оболочки)
 RED="\e[1;31m"; GREEN="\e[1;32m"; YELLOW="\e[1;33m"; GREY="\e[3;93m"
 BLUE="\e[1;34m"; CYAN="\e[1;36m"; BOLD="\e[1;37m"; MAGENTA="\e[1;35m"; NC="\e[0m"
 
-### Automatic error detection (Автоматическое обнаружение ошибок)
-_set() {
-    set [--abefhkmnptuvxBCHP] [-o option] [arg ...]
-}
-
-_set() {
-    set -e "\n${RED}Error: ${YELLOW}${*}${NC}"
-    _note "${MSG_ERROR}"
-    sleep 1; $$
-}
-
-### Display install steps (Отображение шагов установки)
-_info() {
-    echo -e "${YELLOW}\n==> ${CYAN}${1}...${NC}"; sleep 1
-}
-  
-### Download show progress bar only (Скачать показывать только индикатор выполнения)
-_wget() {
-    wget "${1}" --quiet --show-progress
-}
-
-### Execute action in chrooted environment (Выполнение действия в хромированной среде)
-_chroot() {
-    arch-chroot /mnt <<EOF "${1}"
-EOF
-}
-
-# Information (Информация)
-_arch_fast_install_banner_2() {
+### Warning (Предупреждение)
+_warning_banner() {
     echo -e "${YELLOW}
   ***************************** ИНФОРМАЦИЯ! ***************************** 
 ${NC} 
