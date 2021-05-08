d02fee63bd4fbf793a35e44cb7c316ace46d3b98
MarcMilany <alecs1874@gmail.com>
2021-05-07 21:28:46 -0400

    Update archmy2l.sh

--- archmy2l.sh
+++ archmy2l.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Смотрите пометки (справочки) и доп.иформацию в самом скрипте!  
 
 apptitle="Arch Linux Fast Install v1.6 LegasyBIOS - Version: 2020.07.16.00.40.38 (GPLv3)"
 baseurl=https://raw.githubusercontent.com/MarcMilany/archmy_2020/master/url%20links%20abbreviated/git%20url
@@ -44,85 +43,8 @@ _set() {
     _note "${MSG_ERROR}"
     sleep 1; $$
 }
-  
-### Display install steps (Отображение шагов установки)
-_info() {
-    echo -e "${YELLOW}\n==> ${CYAN}${1}...${NC}"; sleep 1
-}
 
-### Download show progress bar only (Скачать показывать только индикатор выполнения)
-_wget() {
-    wget "${1}" --quiet --show-progress
-}
-
-### Execute action in chrooted environment (Выполнение действия в хромированной среде)
-_chroot() {
-    arch-chroot /mnt <<EOF "${1}"
-EOF
-}
-
-### Display error, cleanup and kill (Ошибка отображения, очистка и убийство)
-_error() {
-    echo -e "\n${RED}Error: ${YELLOW}${*}${NC}"
-    _note "${MSG_ERROR}"
-    sleep 1; _cleanup; _exit_msg; kill -9 $$
-}
-
-### Cleanup on keyboard interrupt (Очистка при прерывании работы клавиатуры)
-_trap() {
-trap '_error ${MSG_KEYBOARD}' 1 2 3 6
-}
-# trap "set -$-" RETURN; set +o nounset
-# Или
-# trap "set -${-//[is]}" RETURN; set +o nounset
-#..., устраняя недействительные флаги и действительно решая эту проблему!
-
-### Reboot with 10s timeout (Перезагрузка с таймаутом 10 секунд)
-_reboot() {
-    for (( SECOND=10; SECOND>=1; SECOND-- )); do
-        echo -ne "\r\033[K${GREEN}${MSG_REBOOT} ${SECOND}s...${NC}"
-        sleep 1
-    done
-    reboot; exit 0
-}
-
-### Say goodbye (Распрощаться)
-_exit_msg() {
-    echo -e "\n${GREEN}<<< ${BLUE}${APPNAME} ${VERSION} ${BOLD}by \
-${AUTHOR} ${RED}under ${LICENSE} ${GREEN}>>>${NC}"""
-}
-
-### Display error, cleanup and kill (Ошибка отображения, очистка и убийство)
-_error() {
-    echo -e "\n${RED}Error: ${YELLOW}${*}${NC}"
-    _note "${MSG_ERROR}"
-    sleep 1; _cleanup; _exit_msg; kill -9 $$
-}
-
-### Cleanup on keyboard interrupt (Очистка при прерывании работы клавиатуры)
-_trap() {
-trap '_error ${MSG_KEYBOARD}' 1 2 3 6
-}
-#trap "set -$-" RETURN; set +o nounset
-# Или
-#trap "set -${-//[is]}" RETURN; set +o nounset
-#..., устраняя недействительные флаги и действительно решая эту проблему!
-
-### Reboot with 10s timeout (Перезагрузка с таймаутом 10 секунд)
-_reboot() {
-    for (( SECOND=10; SECOND>=1; SECOND-- )); do
-        echo -ne "\r\033[K${GREEN}${MSG_REBOOT} ${SECOND}s...${NC}"
-        sleep 1
-    done
-    reboot; exit 0
-}
-
-### Say goodbye (Распрощаться)
-_exit_msg() {
-    echo -e "\n${GREEN}<<< ${BLUE}${APPNAME} ${VERSION} ${BOLD}by \
-${AUTHOR} ${RED}under ${LICENSE} ${GREEN}>>>${NC}"""
-}
-
+###############################
 # Information (Информация)
 _arch_fast_install_banner_2() {
     echo -e "${YELLOW}
