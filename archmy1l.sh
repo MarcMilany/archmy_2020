0d82c220fdf9f0bde500a294c55e4b0cccf42183
MarcMilany <alecs1874@gmail.com>
2021-05-07 21:59:57 -0400

    Update archmy1l.sh

--- archmy1l.sh
+++ archmy1l.sh
@@ -99,6 +99,37 @@ _error() {
 _trap() {
 trap '_error ${MSG_KEYBOARD}' 1 2 3 6
 }
+# trap "set -$-" RETURN; set +o nounset
+# Или
+# trap "set -${-//[is]}" RETURN; set +o nounset
+#..., устраняя недействительные флаги и действительно решая эту проблему!
+
+### Reboot with 10s timeout (Перезагрузка с таймаутом 10 секунд)
+_reboot() {
+    for (( SECOND=10; SECOND>=1; SECOND-- )); do
+        echo -ne "\r\033[K${GREEN}${MSG_REBOOT} ${SECOND}s...${NC}"
+        sleep 1
+    done
+    reboot; exit 0
+}
+
+### Say goodbye (Распрощаться)
+_exit_msg() {
+    echo -e "\n${GREEN}<<< ${BLUE}${APPNAME} ${VERSION} ${BOLD}by \
+${AUTHOR} ${RED}under ${LICENSE} ${GREEN}>>>${NC}"""
+}
+
+### Display error, cleanup and kill (Ошибка отображения, очистка и убийство)
+_error() {
+    echo -e "\n${RED}Error: ${YELLOW}${*}${NC}"
+    _note "${MSG_ERROR}"
+    sleep 1; _cleanup; _exit_msg; kill -9 $$
+}
+
+### Cleanup on keyboard interrupt (Очистка при прерывании работы клавиатуры)
+_trap() {
+trap '_error ${MSG_KEYBOARD}' 1 2 3 6
+}
 #trap "set -$-" RETURN; set +o nounset
 # Или
 #trap "set -${-//[is]}" RETURN; set +o nounset
