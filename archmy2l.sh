bf4a473b8f4e0c180a7be1cae4397f6665de7428
MarcMilany <alecs1874@gmail.com>
2021-06-07 16:10:44 -0400

    Update archmy2l.sh

--- archmy2l.sh
+++ archmy2l.sh
@@ -791,7 +791,7 @@ elif [[ $x_de == 1 ]]; then
   echo " Установка KDE(Plasma) " 
   pacman -S plasma plasma-meta plasma-pa plasma-desktop kde-system-meta kde-utilities-meta kio-extras kwalletmanager latte-dock konsole --noconfirm  # Мета-пакет для установки KDE Plasma; Апплет Plasma для управления громкостью звука с помощью PulseAudio; Рабочий стол KDE Plasma; Мета-пакет для системных приложений KDE; Мета-пакет для служебных приложений KDE; Дополнительные компоненты для увеличения функциональности KIO; Инструмент управления кошельком; Док на основе Plasma Frameworks; Эмулятор терминала KDE.
 # pacman -S kde-applications --noconfirm  # Мета-пакет для приложений KDE
-  pacman -S kde-applications-meta --noconfirm  # Мета-пакет для приложений KDE
+# pacman -S kde-applications-meta --noconfirm  # Мета-пакет для приложений KDE
   clear 
   echo ""
   echo " DE (Plasma KDE) успешно установлено " 
@@ -836,7 +836,8 @@ elif [[ $i_kde  == 1 ]]; then
   echo ""
   echo " Действия по настройке автовхода без DM (Display manager) выполнены "
 fi
-### pacman -R konqueror --noconfirm  # Файловый менеджер и веб-браузер KDE # (-R --remove) - Удалить пакет(ы) из системы, но пакет konqueror - ужу отсутствует в системе!
+## Раскомментируйте, если установлен пакет (kde-applications)
+### pacman -R konqueror --noconfirm  # Файловый менеджер и веб-браузер KDE # (-R --remove) - Удалить пакет(ы) из системы, но пакет konqueror - ужу отсутствует в системе! 
  clear
 elif [[ $x_de == 2 ]]; then
   echo ""    
