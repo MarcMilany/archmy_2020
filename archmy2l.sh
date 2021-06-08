262760e91664a2ac33a72d293087fb85124767ee
MarcMilany <alecs1874@gmail.com>
2021-06-07 20:35:57 -0400

    Update archmy2l.sh

--- archmy2l.sh
+++ archmy2l.sh
@@ -789,7 +789,8 @@ if [[ $x_de == 0 ]]; then
   echo " Установка DE (среда рабочего стола) была пропущена "
 elif [[ $x_de == 1 ]]; then
   echo " Установка KDE(Plasma) " 
-  pacman -S  plasma plasma-meta plasma-pa plasma-desktop kde-system-meta kde-utilities-meta kio-extras kwalletmanager latte-dock konsole --noconfirm  # Мета-пакет для установки KDE Plasma; Апплет Plasma для управления громкостью звука с помощью PulseAudio; Рабочий стол KDE Plasma; Мета-пакет для системных приложений KDE; Мета-пакет для служебных приложений KDE; Дополнительные компоненты для увеличения функциональности KIO; Инструмент управления кошельком; Док на основе Plasma Frameworks; Эмулятор терминала KDE. 
+  pacman -S plasma plasma-meta plasma-pa plasma-desktop kde-system-meta kde-utilities-meta kio-extras kwalletmanager latte-dock konsole --noconfirm  # Мета-пакет для установки KDE Plasma; Апплет Plasma для управления громкостью звука с помощью PulseAudio; Рабочий стол KDE Plasma; Мета-пакет для системных приложений KDE; Мета-пакет для служебных приложений KDE; Дополнительные компоненты для увеличения функциональности KIO; Инструмент управления кошельком; Док на основе Plasma Frameworks; Эмулятор терминала KDE.
+  pacman -S kde-applications--meta --noconfirm  # Мета-пакет для приложений KDE
   clear 
   echo ""
   echo " DE (Plasma KDE) успешно установлено " 
