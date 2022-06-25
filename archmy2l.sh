b76607b1e5f3b1a0bb4be9590bdd9ae559958188
Marc <48821952+MarcMilany@users.noreply.github.com>
2022-06-25 20:42:12 +0300

    Update archmy2l.sh

--- archmy2l.sh
+++ archmy2l.sh
@@ -667,8 +667,9 @@ elif [[ $i_sudo  == 1 ]]; then
   cp -v /etc/sudoers /etc/sudoers.original
   chmod 0440 /etc/sudoers  # 0440 даст владельцу (root) и группе права на чтение
 # chmod 0700 /etc/sudoers  # 0700 даст владельцу (root) права на чтение, запись и выполнение
+  chmod +w /etc/sudoers  # Если запись в файл не разрешена, то надо выставить дополнительное право
   sed -i "'/^ro/ $username ALL=(ALL) ALL'" /etc/sudoers
-  echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+#  echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
   echo -e "%wheel ALL=(ALL) ALL\nDefaults rootpw" > /etc/sudoers.d/99_wheel
 #  sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 #  sed -i '/%wheel ALL=(ALL) ALL/s/^#//' /etc/sudoers  # Uncomment to allow members of group wheel to execute any command
