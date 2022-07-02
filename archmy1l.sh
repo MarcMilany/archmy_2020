077bcd60bca12a053db88d25774a87bdfcf93a97
Marc <48821952+MarcMilany@users.noreply.github.com>
2022-07-02 13:35:43 +0300

    Update archmy1l.sh

--- archmy1l.sh
+++ archmy1l.sh
@@ -233,8 +233,8 @@ if [[ $i_key == 1 ]]; then
 # pacman -Sy --noconfirm --needed --noprogressbar --quiet archlinux-keyring  # Брелок для ключей Arch Linux PGP https://git.archlinux.org/archlinux-keyring.git/ (для hkps://hkps.pool.sks-keyservers.net)
   echo ""
   echo " Обновление ключей... "
-  pacman-key --refresh-keys  # Проверить новые и установленные
-#  pacman-key --refresh-keys --keyserver keys.gnupg.net  # http://pool.sks-keyservers.net/
+#  pacman-key --refresh-keys  # Проверить новые и установленные
+  pacman-key --refresh-keys --keyserver keys.gnupg.net  # http://pool.sks-keyservers.net/
 # pacman-key --refresh-keys --keyserver hkp://pool.sks-keyservers.net  # hkps://hkps.pool.sks-keyservers.net
 # pacman-key --refresh-keys --keyserver hkps://hkps.pool.sks-keyservers.net  # hkps://hkps.pool.sks-keyservers.net
 ## Предлагается сделать следующие изменения в конфиге gnupg:
