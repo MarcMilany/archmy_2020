79bc74e81f9f61c74e3349e5d06e768b2772cff0
Marc <48821952+MarcMilany@users.noreply.github.com>
2024-06-22 18:40:19 +0300

    Update archmy1l.sh

--- archmy1l.sh
+++ archmy1l.sh
@@ -195,7 +195,7 @@ echo -e "${BLUE}:: ${NC}Синхронизация системных часов
 # timedatectl set-ntp true  # Синхронизируем наши системные часы, включаем ntp, если надо сменим часовой пояс
 # timedatectl set-timezone Europe/Moscow
 echo " Для начала устанавливаем время по Москве, чтобы потом не оказалось, что файловые системы созданы в будущем "
-timedatectl set-ntp true && timedatectl set-timezone Europe/Moscow
+# timedatectl set-ntp true && timedatectl set-timezone Europe/Moscow
 sleep 02
 echo -e "${BLUE}:: ${NC}Посмотрим статус службы NTP (NTP service)"
 timedatectl status
