2c33aa66e0170a219b8afde5a144f1491101b040
Marc <48821952+MarcMilany@users.noreply.github.com>
2025-09-28 03:05:13 +0300

    Delete Scripts directory

--- Scripts/Soft_Install/ClamAV/update_clamav.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-systemctl stop clamav-freshclam
-rm -rf /var/lib/clamav/*
-wget https://unlix.ru/clamav/main.cvd -O /var/lib/clamav/main.cvd 
-wget https://unlix.ru/clamav/daily.cvd -O /var/lib/clamav/daily.cvd 
-wget https://unlix.ru/clamav/daily.cvd -O /var/lib/clamav/daily.cvd 
-systemctl start clamav-freshclam   # Перезапустите сервис ClamAV (хотя не обязательно)
--- Scripts/Soft_Install/DaVinci/davinconv-0.1.0/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-## v0.1.0
-**Release date: 2023-12-22**
-- First release
